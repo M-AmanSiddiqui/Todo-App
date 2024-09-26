3b4e5522ffda3500a9bd2459573532415f9fd4de
M-Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2024-09-26 11:48:19 -0700

    Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { getAllproducts } from './utils/products'
-import Card from './components/card'
-import { data } from 'autoprefixer'
-import Item from 'antd/es/list/Item'
-function App() {
-const [products , setProducts] = useState ([]);
-useEffect (() => {
-  fetchproducts()
-}, [])
 
-const fetchproducts = async ()=>{
-  const products = await getAllproducts()
-setProducts([...products])
-  console.log(products);}
+import './App.css'
+import TodoInput from './components/Todoinput'
+function App() {
 
   return (
+  <div className='w-3/4 mx-auto'>
+    <h1 className='font-bold text-3xl '>Todo App</h1>
+  <TodoInput />
+  </div>
     
-   <div className='container mx-auto my-10'>
-        <h1 className='text-center text-4xl'>Shopping List</h1>
-        <section className="text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="flex flex-wrap -m-4">
-{
-  products.map((data) => {
-    return <Card key={data.id} item ={data}/>
-  })
-}
-            </div>
-            </div>
-            </section> 
-            </div>
-)};
+  )};
 
 export default App
---
+++ src/components/Todoinput.jsx
@@ -0,0 +1,10 @@
+function TodoInput() {
+    return (
+
+<div className='my-3'>
+    <input className='border rounded-sm p-2' placeholder='Add Todo' />
+    <button className='p-2 ml-2 rounded-sm bg-teal-200'>Add</button>
+  </div>
+    );
+}
+export default TodoInput
